be0b1237c5c81dae45ad517573738b29d730d949
Pedro Henrique <pedro.henrique59@outlook.com>
2024-01-05 16:19:07 -0300

    Implementando JwtConfigurer, JwtTokenFilter e JwtTokenProvider

---
+++ src/main/java/br/com/erudio/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,42 @@
+package br.com.erudio.security.jwt;
+
+import br.com.erudio.exceptions.InvalidJwtAuthenticationException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                if (auth != null) {
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
+        } catch (InvalidJwtAuthenticationException e) {
+            throw new RuntimeException(e);
+        }
+
+        chain.doFilter(request, response);
+    }
+}

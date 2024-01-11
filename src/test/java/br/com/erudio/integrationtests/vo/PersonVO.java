c1db49ef3dcf5cf846bbbc1fc4b637bfcdaed191
Pedro Henrique <pedro.henrique59@outlook.com>
2024-01-11 08:38:15 -0300

    Testes de Integração para AuthController com XML

--- src/test/java/br/com/erudio/integrationtests/vo/PersonVO.java
+++ src/test/java/br/com/erudio/integrationtests/vo/PersonVO.java
@@ -1,10 +1,13 @@
 package br.com.erudio.integrationtests.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@XmlRootElement
 public class PersonVO implements Serializable {
 
     private Long id;

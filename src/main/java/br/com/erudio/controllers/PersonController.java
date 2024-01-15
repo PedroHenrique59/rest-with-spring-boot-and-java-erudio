b5131ad966b008125f4398dd4c57ccf60221747b
Pedro Henrique <pedro.henrique59@outlook.com>
2024-01-15 16:34:58 -0300

    Utilizando paginação nos endpoints de Person e Book

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -11,6 +11,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +30,35 @@ public class PersonController {
 
     @Autowired
     private PersonService personService;
+
+//    @GetMapping(
+//            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
+//    )
+//    @Operation(summary = "Finds all people",
+//            description = "Finds all people",
+//            tags = {"People"},
+//            responses = {
+//                    @ApiResponse(description = "Success", responseCode = "200",
+//                            content = {
+//                                    @Content(mediaType = "application/json",
+//                                            array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))
+//                            }),
+//                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+//                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+//                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+//                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+//            })
+//    public ResponseEntity<Page<PersonVO>> findAll(
+//            @RequestParam(value = "page", defaultValue = "0") Integer page,
+//            @RequestParam(value = "size", defaultValue = "12") Integer size,
+//            @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+//
+//        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+//
+//        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+//
+//        return ResponseEntity.ok(personService.findAll(pageable));
+//    }
 
     @GetMapping(
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
@@ -42,8 +77,46 @@ public class PersonController {
                     @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
             })
-    public List<PersonVO> findAll() {
-        return personService.findAll();
+    public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+
+        return ResponseEntity.ok(personService.findAll(pageable));
+    }
+
+    @GetMapping(value = "/findPersonByName/{firstName}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
+    )
+    @Operation(summary = "Finds people by name",
+            description = "Finds people by name",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
+    public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findPersonsByName(
+            @PathVariable(value = "firstName") String firstName,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+
+        return ResponseEntity.ok(personService.findPersonsByName(firstName, pageable));
     }
 
     @CrossOrigin(origins = "http://localhost:8081")
--- src/main/java/br/com/erudio/services/PersonService.java
+++ src/main/java/br/com/erudio/services/PersonService.java
@@ -10,14 +10,18 @@ import br.com.erudio.mapper.custom.PersonMapper;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.logging.Logger;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
-import java.util.List;
-import java.util.logging.Logger;
 
 @Service
 public class PersonService {
@@ -30,12 +34,35 @@ public class PersonService {
     @Autowired
     private PersonMapper personMapper;
 
-    public List<PersonVO> findAll() {
-        var persons = DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+    @Autowired
+    private PagedResourcesAssembler<PersonVO> assembler;
 
-        persons.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+    public PagedModel<EntityModel<PersonVO>> findAll(Pageable pageable) {
 
-        return persons;
+        var personPage = personRepository.findAll(pageable);
+
+        var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+        personVosPage.map(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+
+        Link link = linkTo(
+                methodOn(PersonController.class)
+                        .findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc")).withSelfRel();
+
+        return assembler.toModel(personVosPage, link);
+    }
+
+    public PagedModel<EntityModel<PersonVO>> findPersonsByName(String firstName, Pageable pageable) {
+
+        var personPage = personRepository.findPersonsByName(firstName, pageable);
+
+        var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+        personVosPage.map(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+
+        Link link = linkTo(
+                methodOn(PersonController.class)
+                        .findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc")).withSelfRel();
+
+        return assembler.toModel(personVosPage, link);
     }
 
     public PersonVO findById(Long id) {

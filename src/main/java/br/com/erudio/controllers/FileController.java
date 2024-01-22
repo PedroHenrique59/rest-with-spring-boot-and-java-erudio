2e8a7fb5f90c616705d20fb0f8693b9d1d6ce0ae
Pedro Henrique <pedro.henrique59@outlook.com>
2024-01-22 10:25:42 -0300

    Adicionando Upload de Arquivos

---
+++ src/main/java/br/com/erudio/controllers/FileController.java
@@ -0,0 +1,43 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.vo.v1.UploadFileResponseVO;
+import br.com.erudio.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
+        var fileName = fileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponseVO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.stream(files).map(this::uploadFile).collect(Collectors.toList());
+    }
+
+}

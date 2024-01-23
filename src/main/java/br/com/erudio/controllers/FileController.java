afc894bfda0b835e754dac98160f21c2732b07b7
Pedro Henrique <pedro.henrique59@outlook.com>
2024-01-23 10:29:45 -0300

    Adicionando Upload de Arquivos

--- src/main/java/br/com/erudio/controllers/FileController.java
+++ src/main/java/br/com/erudio/controllers/FileController.java
@@ -1,24 +1,31 @@
 package br.com.erudio.controllers;
 
 import br.com.erudio.data.vo.v1.UploadFileResponseVO;
+import br.com.erudio.exceptions.MyFileNotFoundException;
 import br.com.erudio.services.FileStorageService;
+import br.com.erudio.services.PersonService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Tag(name = "File Endpoint")
 @RestController
 @RequestMapping("/api/file/v1")
 public class FileController {
+
+    private Logger logger = Logger.getLogger(FileController.class.getName());
 
     @Autowired
     private FileStorageService fileStorageService;
@@ -38,6 +45,27 @@ public class FileController {
     @PostMapping("/uploadMultipleFiles")
     public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
         return Arrays.stream(files).map(this::uploadFile).collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable("fileName") String fileName, HttpServletRequest request) {
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Could not determine file type!");
+        }
+
+        if (contentType.isBlank()) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
--- src/main/java/br/com/erudio/services/FileStorageService.java
+++ src/main/java/br/com/erudio/services/FileStorageService.java
@@ -2,7 +2,10 @@ package br.com.erudio.services;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exceptions.FileStorageException;
+import br.com.erudio.exceptions.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -43,6 +46,20 @@ public class FileStorageService {
             return fileName;
         } catch (Exception e) {
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new MyFileNotFoundException("File not found " + fileName);
+            }
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not found " + fileName, e);
         }
     }
 
